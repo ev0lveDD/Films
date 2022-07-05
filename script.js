52722f82928649774fbd5a76a3578f7ed5205e51
ev0lveDD <102914983+ev0lveDD@users.noreply.github.com>
2022-07-05 14:16:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+var film1 = document.querySelector(".film1");
+var film2 = document.querySelector(".film2");
+var film3 = document.querySelector(".film3");
+var film4 = document.querySelector(".film4");
+
+function toggle1(event) {
+  event.classList.toggle("column-wide");
+  toggleForm(1);
+  smaller(film2);
+  smaller(film3);
+  smaller(film4);
+}
+
+function toggle2(event) {
+  event.classList.toggle("column-wide");
+  toggleForm(2);
+  smaller(film1);
+  smaller(film3);
+  smaller(film4);
+}
+
+function toggle3(event) {
+  event.classList.toggle("column-wide");
+    toggleForm(3);
+    smaller(film1);
+    smaller(film2);
+    smaller(film4);
+}
+
+function toggle4(event) {
+    event.classList.toggle("column-wide");
+    toggleForm(4);
+    smaller(film1);
+    smaller(film2);
+    smaller(film3);
+}
+
+
+function toggleForm(number) {
+  document.querySelector("#f"+number).classList.toggle("invisible");
+}
+
+function smaller(film) {
+  film.classList.toggle("smaller");
+}
+
+function check() {
+  var column = document.getElementsByClassName("column");
+  var i;
+
+  for (i = 0; i < column.length; i++) {
+    column[i].classList.remove("column-wide");
+    column[i].classList.remove("smaller");
+    }
+
+  for (i = 1; i < 4; i++) {
+    document.querySelector("#f"+i).classList.remove("invisible");
+    }
+
+}
